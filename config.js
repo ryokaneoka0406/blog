a722ebc958de251798b192892513415b9665faa2
ryo.kaneoka0406@gmail.com <ryo.kaneoka0406@gmail.com>
2020-04-28 13:05:17 +0900

    add linkedin

--- config.js
+++ config.js
@@ -1,53 +1,53 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://ryopenguin.com',
-  pathPrefix: '/',
-  title: 'ryopenguin blog',
-  subtitle: 'まったり暮らしたいプロダクトマネージャーの日記。',
-  copyright: '© All rights reserved.',
-  disqusShortname: '',
+  url: "https://ryopenguin.com",
+  pathPrefix: "/",
+  title: "ryopenguin blog",
+  subtitle: "まったり暮らしたいプロダクトマネージャーの日記。",
+  copyright: "© All rights reserved.",
+  disqusShortname: "",
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-142748858-2',
+  googleAnalyticsId: "UA-142748858-2",
   useKatex: false,
   menu: [
     {
-      label: 'Articles',
-      path: '/'
+      label: "Articles",
+      path: "/",
     },
     {
-      label: 'About',
-      path: '/pages/about'
+      label: "About",
+      path: "/pages/about",
     },
     {
-      label: 'Portfolio',
-      path: '/pages/portfolio'
+      label: "Portfolio",
+      path: "/pages/portfolio",
     },
     {
-      label: 'Privacy policy',
-      path: '/pages/privacy_policy'
-    }
+      label: "Privacy policy",
+      path: "/pages/privacy_policy",
+    },
   ],
   author: {
-    name: 'Ryo Kaneoka',
-    photo: '/photo.jpg',
-    bio: '個人開発と勉強が趣味のプロダクトマネージャー。まったりくらしたい。',
+    name: "Ryo Kaneoka",
+    photo: "/photo.jpg",
+    bio: "個人開発と勉強が趣味のプロダクトマネージャー。まったりくらしたい。",
     contacts: {
-      email: 'ryo.kaneoka0406@gmail.com',
-      facebook: 'ryo.kaneoka/',
-      telegram: '',
-      twitter: 'ryopenguin',
-      github: 'ryokaneoka0406',
-      rss: '',
-      vkontakte: '',
-      linkedin: '',
-      instagram: 'kaneokanyannyan',
-      line: '',
-      gitlab: '',
-      weibo: '',
-      codepen: '',
-      youtube: '',
-      soundcloud: '',
-    }
-  }
+      email: "ryo.kaneoka0406@gmail.com",
+      facebook: "ryo.kaneoka/",
+      telegram: "",
+      twitter: "ryopenguin",
+      github: "ryokaneoka0406",
+      rss: "",
+      vkontakte: "",
+      linkedin: "ryokaneoka/",
+      instagram: "kaneokanyannyan",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: "",
+      soundcloud: "",
+    },
+  },
 };
